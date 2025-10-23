8b8bdc2741aaa0618c4130ed9b583975ec4b99fb
onlycaidyo <ovianacaique@gmail.com>
2025-10-23 19:49:41 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,29 +1,23 @@
 package POO;
 
-// CLASSE PRINCIPAL
 public class Main {
 
 	public static void main(String[] args) {
-	
-		// Criando vários objetos diferentes
-		Polimorfismo cachorro = new Cachorro();
-		Polimorfismo gato = new Gato();
-		Polimorfismo vaca = new Vaca();
 		
-		// Chamando o mesmo método 'falar' em todos os objetos
-		// Cada objeto executa de forma diferente
-		cachorro.falar();
-		gato.falar();
-		vaca.falar();
-		
-		System.out.println(" --- - --- ");
-		
-		// EXEMPLO COM VETOR DE OBJETOS POLIMÓRFICOS
-		Polimorfismo[] animais = { new Cachorro(), new Gato(), new Vaca() };
-		
-		for (Polimorfismo animal : animais) {
-			animal.falar(); // Mesmo método, comportamentos diferentes
-		}
+		// Criando objetos das classes concretas
+				Abstracao cachorro = new AcaoCachorro();
+				
+				Abstracao gato = new AcaoGato();
+				
+		// Chamando os métodos
+				cachorro.executarAcao();
+				cachorro.mensagemPadrao();
+				
+				System.out.println("------------------------------------------- \n");
+				
+				gato.executarAcao();
+				gato.mensagemPadrao();
+				
 	}
 
 }
